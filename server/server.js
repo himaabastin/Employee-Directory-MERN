ddda5b6f3683868eadd1b2f31e535f08985effb5
himaabastin <111218465+himaabastin@users.noreply.github.com>
2023-02-28 11:20:47 +0530

    for hosting

--- server/server.js
+++ server/server.js
@@ -22,6 +22,9 @@ mongoose.connect(URI, (err) => {
   console.log("Connected to Database");
 });
 
+if(process.env.NODE_ENV ==="production"){
+  app.use(express.static("client/build"))
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
